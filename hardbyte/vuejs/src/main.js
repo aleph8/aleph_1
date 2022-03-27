55fe3f2948b503fec53aecc0de57048184c978e4
Aleph8 <91496152+Aleph8@users.noreply.github.com>
2022-03-27 13:12:59 +0200

    Delete hardbyte directory

--- hardbyte/vuejs/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'bulma/css/bulma.css'; // Framework de CSS usado en el desarrollo
-
-createApp(App).use(router).mount('#app')
