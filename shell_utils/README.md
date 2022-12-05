a07711ef0c5d1742a7e0d5e4bb0bd075807b985d
aleph8 <alegpwk@gmail.com>
2022-12-05 19:56:43 +0100

    Upload new shell util

---
+++ shell_utils/README.md
@@ -0,0 +1,9 @@
+<p align="center"><a href="https://www.alejandrogp.com/"><img hspace="15" width="250" height="250" src="https://github.com/aleph8/aleph8/blob/main/logos/aleph1logo.png?raw=true"></a></p>
+
+<h2 align="center"> Aleph 1 Shell Utils </h2>
+
+<p align="center"> <img src="https://img.shields.io/badge/Shell-Bash-00599C?style=flat-square&logo=shell"></p>
+
+### :heavy_plus_sign: Content
+
+:page_facing_up: **Scripts**  
---
+++ shell_utils/progress.sh
@@ -0,0 +1,63 @@
+##########################################################################
+### aleph8                                                             ###
+###--------------------------------------------------------------------###
+### Description                                                        ###
+###                                                                    ###
+### A set of functions to make the waiting process more visible;       ###
+### you can import the file from your script in the following way:     ###
+###                                                                    ###
+### source <path_to_script>/progress.sh                                ###
+###                                                                    ###
+###--------------------------------------------------------------------###
+###                                                                    ###
+##########################################################################
+
+slash(){
+  tput civis
+  stty -echo
+  $1 & pid=$(echo $!)
+  #echo $pid
+  chars="\ | / -"
+  num=$((${#2}+3))
+  reload=$(printf '\\b%.0s' $(seq 1 $num))
+
+  while kill -0 "$pid" 2> /dev/null ; do
+
+    for i in $chars
+    do
+      sleep 0.5
+      echo -ne $reload"["$i"]"$2
+    done
+
+  done
+
+  echo -e $3
+  tput cnorm
+  stty echo
+
+}
+
+upper(){
+
+  tput civis
+  stty -echo
+  $1 & pid=$(echo $!)
+  #echo $pid
+  num=$((${#2}+3))
+  reload=$(printf '\\b%.0s' $(seq 1 $num))
+
+  while kill -0 "$pid" 2> /dev/null ; do
+
+     for i in $(seq 0 $num)
+     do
+       char=${2:$i:1}
+       sleep 0.1
+       echo -ne $reload"[!]"${2:0:$((0+$i))}${char^^}${2:$(($i+1)):$num}
+     done
+
+   done
+
+  echo -e $3
+  tput cnorm
+  stty echo
+}
